9b042eb2ab981c3c4269d5f63c90bacd8882d65b
Nathan LaPré <nmlapre@gmail.com>
2015-11-07 19:14:25 -0800

    added server code for backend

---
+++ server.js
@@ -0,0 +1,31 @@
+var http = require("http");
+var Firebase = require("firebase");
+var qs = require('querystring');
+var dbref = new Firebase('https://scorching-inferno-7288.firebaseio.com/');
+
+http.createServer(function (request, response) {
+    if (request.method == 'POST') {
+        var body = '';
+
+        request.on('data', function (data) {
+            body += data;
+
+            // Too much POST data, kill the connection!
+            // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+            if (body.length > 1e6)
+                request.connection.destroy();
+        });
+
+        request.on('end', function () {
+            var post = qs.parse(body);
+            // use post['blah'], etc.
+            console.log(request);
+            dbref.set(post);
+        });
+    }
+    if (request.method == 'GET') {
+        var body = '';
+        console.log("test");
+    }
+}
+).listen(3000);
